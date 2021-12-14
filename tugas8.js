6f7afa61a6b42aea4d020a8a377d5b9172101064
Muhammad Aminudin <aminudin199809@gmail.com>
2021-12-15 00:05:00 +0700

    Tugas 8 Js
    Mohon untuk direview kembali

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function siswaSd() {
+  siswa = {
+    nama: "Anndi",
+    umur: 7,
+    kelas: 2,
+    alamat: "Pasar pagi",
+  };
+  console.log(siswa);
+  siswa.alamat = "Pasar Senin";
+  console.log(siswa.alamat);
+}
+siswaSd();
